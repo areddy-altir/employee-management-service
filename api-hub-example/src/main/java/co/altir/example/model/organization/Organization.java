5867c61efa9385c30d2bf1829fc285853cdd45ac
areddy-altir <areddy@altir.co>
2026-02-03 22:09:38 +0530

    Add address model and update organization schema

--- api-hub-example/src/main/java/co/altir/example/model/organization/Organization.java
+++ api-hub-example/src/main/java/co/altir/example/model/organization/Organization.java
@@ -1,6 +1,8 @@
 package co.altir.example.model.organization;
 
 import co.altir.dbmanagement.dataaccess.openapidsl.schema.annotations.DslJpaEntity;
+import co.altir.dbmanagement.dataaccess.openapidsl.schema.annotations.DslRequired;
+import co.altir.example.model.common.Address;
 import co.altir.example.model.common.BaseAuditFields;
 import java.util.List;
 import java.util.UUID;
@@ -15,11 +17,9 @@ public class Organization extends BaseAuditFields {
 
   private UUID id;
 
-  private String name;
+  @DslRequired() private String name;
 
-  private String address;
-
-  private String country;
+  @DslRequired() private Address address;
 
   private List<Employee> employees;
 }
