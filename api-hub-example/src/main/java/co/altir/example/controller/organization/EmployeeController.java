8ebad622598d39520fcc598763649f13314f5e79
areddy-altir <areddy@altir.co>
2026-02-12 11:17:30 +0530

    manager creation

--- api-hub-example/src/main/java/co/altir/example/controller/organization/EmployeeController.java
+++ api-hub-example/src/main/java/co/altir/example/controller/organization/EmployeeController.java
@@ -2,7 +2,10 @@ package co.altir.example.controller.organization;
 
 import co.altir.dbmanagement.dataaccess.openapidsl.endpoint.HttpMethod;
 import co.altir.dbmanagement.dataaccess.openapidsl.endpoint.annotations.DslCrudEndpoints;
+import co.altir.dbmanagement.dataaccess.openapidsl.endpoint.annotations.DslEndpoint;
 import co.altir.example.model.organization.Employee;
+import co.altir.example.model.organization.Manager;
+import java.util.UUID;
 
 // Source cell: Organization (organization)::A24
 public interface EmployeeController {
@@ -11,4 +14,13 @@ public interface EmployeeController {
       path = "/employee",
       methods = {HttpMethod.GET_BY_ID, HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE})
   Employee crudEndpointsForEmployee();
+
+  @DslEndpoint(path = "/employees/{employeeId}/manager", method = HttpMethod.GET)
+  Manager getEmployeeManager(UUID employeeId);
+
+  @DslEndpoint(path = "/employees/{employeeId}/manager", method = HttpMethod.PUT)
+  Manager updateEmployeeManager(UUID employeeId);
+
+  @DslEndpoint(path = "/employees/{employeeId}/hierarchy", method = HttpMethod.GET)
+  Object getEmployeeHierarchy(UUID employeeId);
 }
---
+++ src/main/java/com/example/employeemanagement/controller/ManagerController.java
@@ -0,0 +1,4 @@
+package com.example.employeemanagement.controller;
+
+public class ManagerController {
+}
